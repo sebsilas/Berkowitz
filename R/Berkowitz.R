4b514e4be2dc923d0584517708504b55153b643b
syntheso <sebsilas@gmail.com>
2022-01-26 16:29:32 +0100

    Initial commit

---
+++ R/Berkowitz.R
@@ -0,0 +1,12 @@
+#' Berkowitz Sight-Singing Database
+#'
+#' A dataset containing just over x melodies designed to improve sight-singing
+#'
+#' @format A function containing different versions of the Berkowitz data frame, returned with "main", "ngram", "phrases" and "files" keys
+#' \describe{
+#'   \item{melody}{price, in US dollars}
+#'   \item{durations}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{DOI:10.2307/3389405}
+"Berkowitz"
