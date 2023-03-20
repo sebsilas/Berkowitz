887442e6cb725fc980f4d8c7d0fc57da25d27ef8
sebsilas <sebsilas@gmail.com>
2023-03-20 13:47:14 +0100

    New itembankr style item_banks

--- R/Berkowitz.R
+++ R/Berkowitz.R
@@ -1,12 +1,49 @@
-#' Berkowitz Sight-Singing Database
+
+# TODO. Properly describe DFs (each is different format/length etc.)
+
+
+#' Berkowitz Sight-Singing Item Bank (combined)
 #'
-#' A dataset containing just over x melodies designed to improve sight-singing
+#' A dataset containing ~630 melodies designed to improve sight-singing (combined N-grams and phrases)
 #'
-#' @format A function containing different versions of the Berkowitz data frame, returned with "main", "ngram", "phrases" and "files" keys
-#' \describe{
-#'   \item{melody}{price, in US dollars}
-#'   \item{durations}{weight of the diamond, in carats}
-#'   ...
-#' }
+#' @format Dataframe.
 #' @source \url{DOI:10.2307/3389405}
-"Berkowitz"
+"combined_item_bank"
+
+
+
+#' Berkowitz Sight-Singing Item Bank (N-gram only)
+#'
+#' A dataset containing ~630 melodies designed to improve sight-singing (N-grams only)
+#'
+#' @format Dataframe.
+#' @source \url{DOI:10.2307/3389405}
+"ngram_item_bank"
+
+
+#' Berkowitz Sight-Singing Database (phrases only)
+#'
+#' A dataset containing ~630 melodies designed to improve sight-singing
+#'
+#' @format Dataframe.
+#' @source \url{DOI:10.2307/3389405}
+"phrase_item_bank"
+
+
+
+#' Berkowitz Sight-Singing Database (item version only)
+#'
+#' A dataset containing ~630 melodies designed to improve sight-singing
+#'
+#' @format Dataframe.
+#' @source \url{DOI:10.2307/3389405}
+"item_item_bank"
+
+#' Berkowitz Sight-Singing Database (file version)
+#'
+#' A dataset containing ~630 melodies designed to improve sight-singing
+#'
+#' @format Dataframe.
+#' @source \url{DOI:10.2307/3389405}
+"file_item_bank"
+
--- data-raw/Berkowitz.R
+++ data-raw/Berkowitz.R
@@ -1,81 +1,96 @@
 
+
 library(tidyverse)
 library(itembankr)
 
+setwd('/Users/sebsilas/Berkowitz/data-raw/')
+
 # Test with small
 
-Berkowitz <- corpus_to_item_bank(name = "Berkowitz",
-                               midi_file_dir = "/Users/sebsilas/Berkowitz/data-raw/berkowitz_midi_rhythmic_100bpm_small",
-                               musicxml_file_dir = "/Users/sebsilas/Berkowitz/data-raw/berkowitz_musicxml_small",
-                               input = "files",
-                               output = "all")
+create_item_bank(name = "Berkowitz_test",
+                   midi_file_dir = "/Users/sebsilas/Berkowitz/data-raw/berkowitz_midi_rhythmic_100bpm_small",
+                   musicxml_file_dir = "/Users/sebsilas/Berkowitz/data-raw/berkowitz_musicxml_small",
+                   input = "files",
+                   output = "all")
 
 
-#Berkowitz("files")
+
+load(file = 'Berkowitz_test_combined.rda')
+load(file = 'Berkowitz_test_file.rda')
+load(file = 'Berkowitz_test_item.rda')
+load(file = 'Berkowitz_test_ngram.rda')
+load(file = 'Berkowitz_test_phrase.rda')
+
+is(combined_item_bank, "item_bank")
+
+# See which compression method wins
+# save(combined_item_bank, file = "Berkowitz_test_gzip.rda", compress = "gzip")
+# save(combined_item_bank, file = "Berkowitz_test_bzip2.rda", compress = "bzip2")
+# save(combined_item_bank, file = "Berkowitz_test_xz.rda", compress = "xz")
+
+# xz wins (same found with WJD)
 
 
-remove_tags <- function(string, tag) {
-  stringr::str_remove(string, paste0("<",tag,">")) %>%
-    stringr::str_remove(., paste0("</",tag,">")) %>%
-    gsub(" ", "", .)
-}
+
+# Create real df
 
 
-grab_first_note_of_music_xml <- function(f) {
-  f <- system.file(f, package = "Berkowitz")
-  t <- readLines(f)
-  step <- remove_tags(t[which.min(!grepl("step", t))], "step")
-  octave <- remove_tags(t[which.min(!grepl("octave", t))], "octave")
-  sci_no <- paste0(step, octave)
-}
-
-Berkowitz_files_with_first_note <- Berkowitz("files") %>%
-  mutate(first_note = stringr::str_replace(musicxml_file,
-                                           "item_banks",
-                                           "extdata"),
-  first_note = purrr::map(first_note, grab_first_note_of_music_xml),
-  first_note_midi = itembankr::sci_notation_to_midi(first_note))
+create_item_bank(name = "Berkowitz",
+                 midi_file_dir = "/Users/sebsilas/Berkowitz/data-raw/berkowitz_midi_rhythmic_100bpm",
+                 musicxml_file_dir = "/Users/sebsilas/Berkowitz/data-raw/berkowitz_musicxml",
+                 input = "files",
+                 output = "all")
 
 
-usethis::use_data(Berkowitz_files_with_first_note, overwrite = TRUE)
 
 
-# add i.entropy
+load('Berkowitz_ngram.rda')
+load('Berkowitz_combined.rda')
+load('Berkowitz_phrase.rda')
+load('Berkowitz_item.rda')
+load('Berkowitz_file.rda')
 
-getwd()
-load('data/Berkowitz.rda')
-phr.length.limits <- c(2,140)
+use_data(combined_item_bank,
+     ngram_item_bank,
+     phrase_item_bank,
+     item_item_bank,
+     file_item_bank,
+     compress = "xz", overwrite = TRUE)
 
-files_db <- Berkowitz("files")
 
-ngram_db <- Berkowitz("ngram") %>%
-  rowwise() %>%
-  mutate(i.entropy = compute.entropy(itembankr::rel_to_abs_mel(str_mel_to_vector(melody)), phr.length.limits[2]-1)) %>%
-  ungroup()
 
-hist(ngram_db$i.entropy)
 
-main_db <- Berkowitz("main")  %>%
-  rowwise() %>%
-  mutate(i.entropy = compute.entropy(itembankr::rel_to_abs_mel(str_mel_to_vector(melody)), phr.length.limits[2]-1)) %>%
-  ungroup()
 
-hist(main_db$i.entropy)
+#
+# # Add first note
+#
+# remove_tags <- function(string, tag) {
+#   stringr::str_remove(string, paste0("<",tag,">")) %>%
+#     stringr::str_remove(., paste0("</",tag,">")) %>%
+#     gsub(" ", "", .)
+# }
+#
+#
+# grab_first_note_of_music_xml <- function(f) {
+#   f <- system.file(f, package = "Berkowitz")
+#   t <- readLines(f)
+#   step <- remove_tags(t[which.min(!grepl("step", t))], "step")
+#   octave <- remove_tags(t[which.min(!grepl("octave", t))], "octave")
+#   sci_no <- paste0(step, octave)
+# }
+#
+#
+# Berkowitz_files_with_first_note <- Berkowitz("files") %>%
+#   mutate(first_note = stringr::str_replace(musicxml_file,
+#                                            "item_banks",
+#                                            "extdata"),
+#          first_note = purrr::map(first_note, grab_first_note_of_music_xml),
+#          first_note_midi = itembankr::sci_notation_to_midi(first_note))
+#
+#
+# usethis::use_data(Berkowitz_files_with_first_note, overwrite = TRUE)
 
-phrases_db <- Berkowitz("phrases")  %>%
-  rowwise() %>%
-  mutate(i.entropy = compute.entropy(itembankr::rel_to_abs_mel(str_mel_to_vector(melody)), phr.length.limits[2]-1)) %>%
-  ungroup()
 
-hist(phrases_db$i.entropy)
 
-Berkowitz <- function(key) {
-  l <- list("files" = files_db,
-            "ngram" = ngram_db,
-            "main" = main_db,
-            "phrases" = phrases_db)
-  l[[key]]
-}
 
-use_data(Berkowitz, files_db, ngram_db, main_db, phrases_db, overwrite = TRUE)
 
